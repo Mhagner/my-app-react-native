83084e6c0c76e7d0ba128a0b3d36abef5d91411f
mahilsonhagnner <mahilson.pereira@acct.global>
2023-03-30 23:15:31 -0300

    Shared component

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Share, TouchableOpacity } from 'react-native'
 import styles from './style'
 
 
 export default function ResultImc({ resultImc, messageResultImg }) {
+
+  const onShare = async () => {
+    return await Share.share({
+      message: `Meu imc hoje é de: ${resultImc}`,
+    })
+  }
+
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxSharedButton}>
+        {resultImc != null ?
+          <TouchableOpacity
+            onPress={onShare}
+            style={styles.shared}>
+            <Text style={styles.sharedText}>Compartilhar</Text>
+          </TouchableOpacity>
+          :
+          <View />
+        }
+      </View>
       <Text style={styles.information}>{messageResultImg}</Text>
       <Text style={styles.numberImc}>{resultImc}</Text>
     </View>
--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -18,6 +18,21 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#FF0043',
         fontWeight: 'bold',
+    },
+    boxSharedButton: {
+        width: '100%',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    shared: {
+        backgroundColor: '#1877f2',
+        borderRadius: 50,
+        paddingVertical: 5
+    },
+    sharedText: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+        paddingHorizontal: 30,
     }
 })
 
